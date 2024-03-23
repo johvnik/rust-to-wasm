1ab060ec42e96eacb6ffd1cfa86050c003246889
johvnik <vaught.john@gmail.com>
2024-03-23 01:18:55 +0100

    init

---
+++ web/src/wasm.ts
@@ -0,0 +1,19 @@
+
+const importObject = {
+  imports: {
+    random: () => Math.random(),
+  },
+};
+
+const wasm = await fetch("core.wasm");
+const { instance: core } = await WebAssembly.instantiateStreaming(wasm, importObject);
+
+// @ts-ignore: add is function
+const output: number = core.exports.add(40, 2);
+console.log(output);
+console.log(core.exports.memory);
+console.log(core.exports.__data_end);
+console.log(core.exports.__heap_base);
+
+// needed to coerce into a module
+export { }
